b5936637c9c9960c3455725819549c26edebc1b6
shruthikurva <kurvashruthi98@gmail.com>
2025-01-30 21:57:04 +0530

    Add files via upload

---
+++ Prime.java
@@ -0,0 +1,23 @@
+     public class Prime {
+      public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int number = 4;
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+} 
+    
+
